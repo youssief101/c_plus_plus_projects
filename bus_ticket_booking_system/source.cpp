30fda5b9880327fe9ab5b906044907095871ab1f
youssief101 <30203051305119@sci.asu.edu.eg>
2023-10-04 10:32:03 +0300

    solving a problem in the code

--- bus_ticket_booking_system/source.cpp
+++ bus_ticket_booking_system/source.cpp
@@ -53,7 +53,7 @@ int getUserChoice3() {
 
     cin >> ret;
     
-    if (ret >= 0 && ret <= 3)
+    if (ret >= 5 && ret <= 7)
       return ret;
     else
       cerr << "Invalid Input, try again ..." << endl;
